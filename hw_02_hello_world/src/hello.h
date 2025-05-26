486cfaff896286c65cb612cc4922c0403924fcbc
SergSentry <rtfm.faq@mail.ru>
2025-05-26 23:11:41 +0300

    hw_02_hello_world: add source

---
+++ hw_02_hello_world/src/hello.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/**
+ * OTUS course homework 'Linux Kernel Development'
+ * Task: HW_02_hello_world
+ * 
+ * Copyright (c) 2007-2025 SergSentry
+ */
+
+#ifndef HELLO_H
+#define HELLO_H
+
+MODULE_DESCRIPTION("OTUS course homework 'Linux Kernel Development'\n\t\tTask: "
+		   "HW_02_hello_world");
+
+MODULE_AUTHOR("SergSentry");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+
+#endif
