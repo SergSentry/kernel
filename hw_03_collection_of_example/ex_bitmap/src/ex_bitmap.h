33eef745b5fb611da4d451ed9cb0aa2bed5ec15a
SergSentry <rtfm.faq@mail.ru>
2025-06-24 20:40:10 +0300

    hw_03_collection_of_example: add ex_bitmap

---
+++ hw_03_collection_of_example/ex_bitmap/src/ex_bitmap.h
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/**
+ * OTUS course homework 'Linux Kernel Development'
+ * Task: HW_03_collection_of_example
+ * Module: ex_bitmap
+ * 
+ * Copyright (c) 2007-2025 SergSentry
+ */
+
+#ifndef EX_BITMAP_H
+#define EX_BITMAP_H
+
+#define BITMAP_SIZE (32)
+
+#define MAX_CMD_STR_LEN (10)
+#define MAX_CMD_ARG_STR_LEN (50)
+#define MAX_CMD_PARAM_LEN ((MAX_CMD_STR_LEN) + (MAX_CMD_ARG_STR_LEN))
+
+#define STATUS_NONE (-EINVAL)
+#define STATUS_UNKNOWN_CMD (-EINVAL)
+#define STATUS_OK (0)
+#define STATUS_CMD_ERROR_ARG (-EINVAL)
+#define STATUS_CMD_ERROR_MEMORY (-ENOMEM)
+
+#define COMMAND(NAME) { #NAME, NAME##_cmd_handler }
+
+struct command {
+	char *name;
+	int (*handle)(const char *argv);
+};
+
+MODULE_DESCRIPTION("OTUS course homework 'Linux Kernel Development'\n\t\tTask: "
+		   "HW_03_collection_of_example\n\t\tModule: ex_bitmap");
+
+MODULE_AUTHOR("SergSentry");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+
+#endif
